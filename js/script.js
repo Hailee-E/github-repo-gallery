f417c2c9f508c5f627a97fec859b66f43412c1cd
Hailee E <herikson24@hotmail.com>
2022-05-12 18:10:18 -0700

    Fixed Unwanted Space

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const displayUserInfo = function (data) {
         <p><strong>Name:</strong> ${data.name}</p>
         <p><strong>Bio:</strong> ${data.bio}</p>
         <p><strong>Location:</strong> ${data.location}</p>
-        <p><strong>Number of public repos:</strong>${data.public_repos}</p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>
     `;
     overview.append(div);
